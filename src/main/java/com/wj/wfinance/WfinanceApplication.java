ef34af9ddc210c338489b430de167ba66a644cc1
Walter | Corporativo <walterjuniorcorporativo@gmail.com>
2026-06-02 18:23:38 -0300

    Init commit

---
+++ src/main/java/com/wj/wfinance/WfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.wj.wfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wj/wfinance/WfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wj.wfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
